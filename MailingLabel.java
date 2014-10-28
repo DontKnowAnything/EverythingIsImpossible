8477e50f7277ea839e66f8161e5b055cebaf3ee6
DontKnowAnything <pcmcknew1@mymail.aacc.edu>
2014-10-28 14:45:37 -0400

    Update MailingLabel.java

--- MailingLabel.java
+++ MailingLabel.java
@@ -1,4 +1,17 @@
+import java.io.Serializable;
 
-public class MailingLabel
-{
+/**
+ * This class represents a mailing label. It implements the serializable
+ * interface. Its data members (name, address, city, state, and zip code) into a
+ * single String parameter. It uses split method of the String class to parse
+ * the String into data members. Each field has accessor and mutator methods.
+ * The toString method displays the 5 data members concatenated in mailing label
+ * format. If the parameter String does not contain exactly 5 fields, then the
+ * constructor would throw an IllegalMailingLabelException.
+ * 
+ * @author Patrick Mc Knew
+ *
+ */
+public class MailingLabel implements Serializable {
+
 }
